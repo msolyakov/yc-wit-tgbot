28fb4e9ded0d8b8b43628940b71fe303de73082d
Mikhail Solyakov <mikhail.solyakov@gmail.com>
2020-03-10 12:09:11 +0400

    Change dist dir to _dist

--- build/pack.cmd
+++ build/pack.cmd
@@ -5,25 +5,25 @@ set mm=%DATE:~3,2%
 set yyyy=%DATE:~6,4%
 set curdate=%dd%-%mm%-%yyyy%
 
-rd /s /q dist
-mkdir dist
+rd /s /q _dist
+mkdir _dist
 
 rem Coping project dependencies
-pipenv lock -r > dist\reqs.txt
-pip3 install -r dist\reqs.txt --target dist\ 
+pipenv lock -r > _dist\reqs.txt
+pip3 install -r _dist\reqs.txt --target _dist\ 
 
 rem Coping project files and folders
-copy main.py dist\ /y
-xcopy telebot dist\telebot\ /s /e /y /exclude:build\xcld.txt
-xcopy yacloud dist\yacloud\ /s /e /y /exclude:build\xcld.txt
-xcopy wit dist\wit\ /s /e /y /exclude:build\xcld.txt
+copy main.py _dist\ /y
+xcopy telebot _dist\telebot\ /s /e /y /exclude:build\xcld.txt
+xcopy yacloud _dist\yacloud\ /s /e /y /exclude:build\xcld.txt
+xcopy wit _dist\wit\ /s /e /y /exclude:build\xcld.txt
 
 rem Delete existing zip
 set destzip=build\pack_%curdate%.zip
 del %destzip%
 
 rem Create output zip
-cd dist
+cd _dist
 "C:\Program Files\7-Zip\7z.exe" a -tzip -r0 ..\%destzip% *.*
 
 rem "C:\Program Files\7-Zip\7z.exe" a -tzip -r0 %destzip% %source%main.py
